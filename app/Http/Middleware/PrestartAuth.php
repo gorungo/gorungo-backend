4f76dd8c304a0b7d5d6cf3aad47d4cbaa7d66a0a
inkas <denis.g.petrov@gmail.com>
2019-05-07 16:56:08 +1000

    add prestart function

---
+++ app/Http/Middleware/PrestartAuth.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Route;
+
+class PrestartAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if($request->getRequestUri() !=='/prestart'){
+            session()->put('prestart', 'prestart');
+        }
+
+        if ($request->getRequestUri() !=='/' && !session()->has('prestart')) {
+            return redirect()->route('index');
+        }
+
+        return $next($request);
+    }
+}
